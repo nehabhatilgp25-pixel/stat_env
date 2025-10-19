b70be10c75f4112e064c4f45f5a2ea6eed762980
nehabhati516 <141733196+nehabhati516@users.noreply.github.com>
2025-10-19 11:24:29 +0530

    files made

---
+++ WemosCode.cpp
@@ -0,0 +1,44 @@
+#include <NinjaIoT.h>
+#include "DHT.h"
+
+NinjaIoT iot;
+
+#define DHTPIN D2      //connecting sensor to pin d2
+#define DHTTYPE DHT11  //type of DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  iot.connect("MSB_2G", "9900015169", "NB09");  //link: https://iot.roboninja.in/
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();     //storing humidity
+  float t = dht.readTemperature();  //storing temperature
+
+  iot.WriteVar("Temperature", t);
+  iot.WriteVar("Humidity", h);
+
+  iot.ReadAll();  //reading all values from cloud
+
+  int GasValue = analogRead(A0);       //getting analog gas value from A0
+  iot.WriteVar("GasValue", GasValue);  // uploading to cloud
+
+  iot.SyncIN("D0");  // D0 doesn't connect to anything, it just outputs whether it's in automatic or manual mode.
+  // 0 = manual
+  // 1 = automatic
+  if (digitalRead(D0) == LOW) {
+    iot.SyncOut("D6");  // Telling LED whether or not to glow.
+  } else {
+    if (t > 28 || t < 18 || h > 60 || h < 40 || GasValue > 2000) {
+      digitalWrite(D6, HIGH);
+    } else {
+      digitalWrite(D6, LOW);
+    }
+  }
+
+  delay(500);
+}
