48331e061b61deebf8934789ffd31e0ea423b298
Uthman <96424036+UthmansALGOS@users.noreply.github.com>
2025-10-19 11:37:33 +0530

    Update WemosCode.cpp

--- WemosCode.cpp
+++ WemosCode.cpp
@@ -1,37 +1,35 @@
 #include <NinjaIoT.h>
-#include <DHT.h>
+#include "DHT.h"
 
 NinjaIoT iot;
-
-#define DHTPIN D2      //connecting sensor to pin d2
-#define DHTTYPE DHT11  //type of DHT sensor
+#define DHTPIN D2
+#define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  iot.connect("wifi", "pwd", "uid");  //link: https://iot.roboninja.in/
+  iot.connect("MSB_2G", "9900015169", "NB09");
+
   dht.begin();
+
+  pinMode(D0, INPUT);
+  pinMode(D6, OUTPUT);
 }
 
 void loop() {
-  float h = dht.readHumidity();     //storing humidity
-  float t = dht.readTemperature();  //storing temperature
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  int GasValue = analogRead(A0);
 
   iot.WriteVar("Temperature", t);
   iot.WriteVar("Humidity", h);
+  iot.WriteVar("GasValue", GasValue);
+  iot.ReadAll();
 
-  iot.ReadAll();  //reading all values from cloud
+  iot.SyncIN("D0");
 
-  int GasValue = analogRead(A0);       //getting analog gas value from A0
-  iot.WriteVar("GasValue", GasValue);  // uploading to cloud
-
-  iot.SyncIN("D0");  // D0 doesn't connect to anything, it just outputs whether it's in automatic or manual mode.
-  // 0 = manual
-  // 1 = automatic
   if (digitalRead(D0) == LOW) {
-    iot.SyncOut("D6");  // Telling LED whether or not to glow.
+    iot.SyncOut("D6");
   } else {
     if (t > 28 || t < 18 || h > 60 || h < 40 || GasValue > 2000) {
       digitalWrite(D6, HIGH);
