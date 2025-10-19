be128cbd9764e556add95686bfc1e962c310f5fd
nehabhatilgp25-pixel <neha.bhati_lgp25@ashoka.edu.in>
2025-10-19 11:33:11 +0530

    Update WemosCode.cpp

--- WemosCode.cpp
+++ WemosCode.cpp
@@ -11,7 +11,7 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  iot.connect("MSB_2G", "9900015169", "NB09");  //link: https://iot.roboninja.in/
+  iot.connect("wifi", "pwd", "uid");  //link: https://iot.roboninja.in/
   dht.begin();
 }
 
